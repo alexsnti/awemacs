3c7a81ff08493d2c4a4cade4dde59dce65539e7e
Alexandre Santini <santini.alex@gmail.com>
2014-10-21 15:45:26 +0200

    First commit

---
+++ elpa/web-mode-20141018.216/web-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; web-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (web-mode) "web-mode" "web-mode.el" (21574 24893
+;;;;;;  659135 484000))
+;;; Generated autoloads from web-mode.el
+
+(autoload 'web-mode "web-mode" "\
+Major mode for editing web templates.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("web-mode-pkg.el") (21574 24893 756827
+;;;;;;  717000))
+
+;;;***
+
+(provide 'web-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; web-mode-autoloads.el ends here
---
+++ init.el
@@ -0,0 +1,80 @@
+;; WELCOME TO AWALEX EMACS CONFIG FILE ;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (cyberpunk)))
+ '(custom-safe-themes (quote ("f0a99f53cbf7b004ba0c1760aa14fd70f2eabafe4e62a2b3cf5cabae8203113b" default)))
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; Font size
+(set-face-attribute 'default nil :height 105)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/cyberpunk-theme.el")
+
+;; binding files to mode
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.twig\\'" . html-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . html-mode))
+(add-to-list 'auto-mode-alist
+             '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . html-mode))
+
+;; disabling toolbar
+(tool-bar-mode -1)
+
+;; Marmalade package 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+
+(setq-default indent-tabs-mode nil)
+
+;; Jedi configuration
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)  
+
+;; IDO configuration, enabling autocomplete MX and FINDFILE 
+(require 'ido)
+(ido-mode 1)
+(add-hook 'find-file-hook 'ido-remember-buffer-file)
+(defun ido-remember-buffer-file ()
+  (interactive)
+  (let ((file (expand-file-name (buffer-file-name))))
+    (if file
+        (let ((dir (file-name-directory file))
+              (name (file-name-nondirectory file)))
+          (ido-record-work-file name)
+          (ido-record-work-directory dir)
+          (ido-file-name-all-completions dir)))))
+(ido-everywhere 1)
+(defadvice completing-read
+  (around use-ido-when-possible activate)
+  (if (not (boundp 'ido-cur-list))
+      (let ((completions (all-completions "" collection predicate)))
+        (if completions
+            (setq ad-return-value
+                  (ido-completing-read prompt completions nil require-match
+                                       initial-input hist def)))))
+  (unless ad-return-value
+    ad-do-it))
+(define-key global-map [(meta ?x)] 'ido-meta-x)
+(defun ido-meta-x ()
+  (interactive)
+  (call-interactively
+   (intern
+    (or (completing-read "M-x " (all-completions "" obarray 'commandp))))))
+;; End of IDO conf
